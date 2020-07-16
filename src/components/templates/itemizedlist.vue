2df3217393dc9631779b9ad8125dda42b75f71a6
Hugh Sorby <h.sorby@auckland.ac.nz>
2020-07-16 15:23:51 +1200

    Add missing templates.

---
+++ src/components/templates/itemizedlist.vue
@@ -0,0 +1,20 @@
+<script>
+import { doxygenChildren } from '@/mixins/DoxygenChildren'
+
+export default {
+  name: 'itemizedlist',
+  mixins: [doxygenChildren],
+  props: {
+    data: {
+      type: Object
+    }
+  },
+  data() {
+    return {
+      tagName: 'ul'
+    }
+  }
+}
+</script>
+
+<style scoped></style>
---
+++ src/components/templates/listitem.vue
@@ -0,0 +1,20 @@
+<script>
+import { doxygenChildren } from '@/mixins/DoxygenChildren'
+
+export default {
+  name: 'listitem',
+  mixins: [doxygenChildren],
+  props: {
+    data: {
+      type: Object
+    }
+  },
+  data() {
+    return {
+      tagName: 'li'
+    }
+  }
+}
+</script>
+
+<style scoped></style>
