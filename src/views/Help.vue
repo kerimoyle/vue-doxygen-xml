9ed1bb292858397fd80bfdb4f38ac3ef477a1f9e
Hugh Sorby <h.sorby@auckland.ac.nz>
2020-07-21 23:31:24 +1200

    Remove commented out code.

--- src/views/Help.vue
+++ src/views/Help.vue
@@ -4,38 +4,7 @@
 
 <script>
 // @ is an alias to /src
-// import { updateDoxygenRoute } from '@/router/modules/doxygen'
 import DoxygenXml from '@/components/DoxygenXml'
-
-// This is horrible relies 100% on the fact that the page has finished loading by the time this fires.
-// const scrollDelay = 500
-
-// const isInViewport = function(elem) {
-//   const bounding = elem.getBoundingClientRect()
-//   return (
-//     bounding.top >= 0 &&
-//     bounding.left >= 0 &&
-//     bounding.bottom <=
-//       (window.innerHeight || document.documentElement.clientHeight) &&
-//     bounding.right <=
-//       (window.innerWidth || document.documentElement.clientWidth)
-//   )
-// }
-
-// const updateRoute = function(to, from, next) {
-//   updateDoxygenRoute(to, next)
-//   if (!from.name && to.hash) {
-//     setTimeout(() => {
-//       const toElem = document.querySelector(to.hash)
-//       if (toElem) {
-//         window.scrollTo({
-//           top: toElem.offsetTop,
-//           behavior: 'smooth'
-//         })
-//       }
-//     }, scrollDelay)
-//   }
-// }
 
 export default {
   name: 'Home',
