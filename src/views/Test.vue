386e780219d92fd681ad3441e599959aad519cf3
Hugh Sorby <h.sorby@auckland.ac.nz>
2020-07-21 22:27:50 +1200

    Add a testing page to look at a loading message.

---
+++ src/views/Test.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="test">
+    <loading name="bob" />
+  </div>
+</template>
+
+<script>
+import Loading from '@/components/Loading'
+
+export default {
+  name: 'Test',
+  components: { Loading }
+}
+</script>
