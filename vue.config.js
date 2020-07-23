8a2b7bf1c4e18d1e337c8bfd79e59d74a97193e2
Hugh Sorby <h.sorby@auckland.ac.nz>
2020-07-23 14:22:37 +1200

    Try and make the built package smaller by making zxios external.

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,14 @@
+function getProdExternals() {
+  return {
+    axios: 'axios'
+  }
+}
+
 module.exports = {
   devServer: {
     host: 'localhost'
+  },
+  configureWebpack: {
+    externals: process.env.NODE_ENV === 'production' ? getProdExternals() : {}
   }
 }
